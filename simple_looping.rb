4b3b0ae95084bca20ebb8dc0e0cc74fcc9d8b56a
Daniel Shimield <dxshim90@gmail.com>
2019-01-28 15:49:42 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -3,7 +3,8 @@
 def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
-
+while counter <= number_of_times
+puts phrase
 end
 
 def times_iterator(number_of_times)
